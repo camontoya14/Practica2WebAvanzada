5604d232cd38935ea6072da4f55723ee65845cd9
camontoya14 <mmontoya20068@ufide.ac.cr>
2024-02-27 20:15:58 -0800

    primer commit

---
+++ Practica2API/Practica2API/Controllers/VendedorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practica2API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendedorController : ControllerBase
+    {
+    }
+}
